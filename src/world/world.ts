dcc8da668b53e77a3a401b8fc67e96761d98baed
vibegamemaker-dev <jason026386@gmail.com>
2025-08-11 13:23:04 +0900

    feat: implement WorldPhysics and WorldRendering classes; integrate them into World class

--- src/world/world.ts
+++ src/world/world.ts
@@ -1,3 +1,7 @@
+import { WorldPhysics } from "./world-physics";
+import { WorldRendering } from "./world-rendering";
+
 export class World {
-  
+  #rendering = new WorldRendering();
+  #physics = new WorldPhysics();
 }
