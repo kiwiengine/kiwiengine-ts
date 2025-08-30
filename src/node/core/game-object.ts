4e190e062acc916a6cb83b37ae3b8c93585cf819
vibegamemaker-dev <jason026386@gmail.com>
2025-08-30 15:13:30 +0900

    feat: initialize GameObject with PIXI Container for enhanced rendering
    - Updated the GameObject class to include a constructor that initializes the parent DisplayNode with a new PIXI Container.
    - This change improves the rendering capabilities of GameObject instances by ensuring they are properly encapsulated within a PIXI container.

--- src/node/core/game-object.ts
+++ src/node/core/game-object.ts
@@ -1,4 +1,9 @@
 import { EventMap } from '@webtaku/event-emitter'
 import { DisplayNode } from "./display-node"
+import { Container } from 'pixi.js'
 
-export class GameObject<E extends EventMap> extends DisplayNode<E> { }
+export class GameObject<E extends EventMap> extends DisplayNode<E> {
+  constructor() {
+    super(new Container())
+  }
+}
